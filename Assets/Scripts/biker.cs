a0e33f4d7ee8ee056cacbf2adbb0fc632b048296
Maynic <zye03@ucsb.edu>
2020-11-29 22:36:00 -0800

    ini

---
+++ Assets/Scripts/biker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class biker : MonoBehaviour
+{
+    private CharacterController controller;
+    // private Vector3 playerVelocity;
+    // private bool groundedPlayer;
+    private float playerSpeed = 24.0f;
+    // private float jumpHeight = 1.0f;
+    // private float gravityValue = -90.81f;
+    // private float timeLimit = 0.0f;
+    // private bool isMoving = false;
+    Vector3 move = Vector3.left;
+    bool canTurn = true;     
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(move * Time.smoothDeltaTime * playerSpeed);
+        if (move != Vector3.zero){ gameObject.transform.forward = move; }
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if(canTurn && col.gameObject.tag == "OtherZone")
+        {
+            move = -1 * move;
+            canTurn = false;
+            StartCoroutine(ExampleCoroutine());
+        }
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        canTurn = true;
+    }
+}
